ab9eb03907a2eaeb1a740771f14cd2fd8d2048a5
Asandei Stefan <stefan.asandei@protonmail.com>
2021-11-30 21:08:59 +0200

    homescreen feed

---
+++ fetch-yt.cjs
@@ -0,0 +1,55 @@
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+const discoverData = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://youtube.com", {
+    waitUntil: "networkidle2",
+  });
+  await page.setViewport({
+    width: 1200,
+    height: 800,
+  });
+  await page.evaluate(() => {
+    window.scrollBy(0, 2000);
+  });
+  await page.waitForNetworkIdle();
+  const content = await page.content();
+  const $ = cheerio.load(content);
+
+  let data = [];
+  const numberOfVideos = 12;
+
+  for (let i = 0; i < numberOfVideos; i++) {
+    data.push({
+      title: "",
+      thumbnail: "",
+      id: "",
+    });
+  }
+
+  $("#video-title").each((index, element) => {
+    if (index >= numberOfVideos) return;
+    data[index].title = $(element).text().trim();
+  });
+
+  $("#thumbnail #img").each((index, element) => {
+    if (index > numberOfVideos) return;
+    if (parseInt($(element).attr("width")) >= 100) {
+      data[index - 1].thumbnail = $(element).attr("src");
+    }
+  });
+
+  $("#thumbnail").each((index, element) => {
+    if (index > numberOfVideos || data[index - 1] == undefined) return;
+    data[index - 1].id = $(element).attr("href").substring(9);
+  });
+
+  await fs.writeFileSync("public/feed.js", JSON.stringify({ data: data }));
+
+  await browser.close();
+};
+
+discoverData();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 import "./css/styles.scss";
+import { get } from "axios";
+import Plyr from "plyr";
 
-console.log("lmao");
+const playVideo = (id) => {
+  document.getElementById("watch").style.display = "";
+  document.getElementById("feed").style.display = "none";
+
+  document.getElementById(
+    "watch"
+  ).innerHTML = `<div id="player" data-plyr-provider="youtube" data-plyr-embed-id="${id}"></div>`;
+
+  const player = new Plyr("#player", {
+    autoplay: true,
+    youtube: {
+      noCookie: true,
+      rel: 0,
+      showinfo: 0,
+      iv_load_policy: 3,
+      modestbranding: 1,
+    },
+    ads: { enabled: false, publisherId: "", tagUrl: "" },
+  });
+};
+
+const main = async () => {
+  document.getElementById("watch").style.display = "none";
+
+  const res = await get("/feed.js");
+  const data = await res.data.data;
+  for (let i = 0; i < data.length; i++) {
+    document.querySelector(`#video-${i}`).innerHTML = `
+    <amp-img [src]="video_${i}" width="720" height="404" layout="responsive" src="${data[i].thumbnail}"></amp-img>
+    <p>${data[i].title}</p>
+    `;
+    document.querySelector(`#video-${i}`).addEventListener("click", () => {
+      playVideo(data[i].id);
+    });
+  }
+
+  let handleMousemove = (event) => {
+    // if (event.y > (window.innerHeight / 3) * 2) {
+    //   window.scroll(0, 200);
+    // }
+    // if (event.y < (window.innerHeight / 3) * 2) {
+    //   window.scroll(0, -200);
+    // }
+    // console.log(`mouse position: ${event.x}:${event.y}`);
+  };
+
+  document.addEventListener("mousemove", handleMousemove);
+};
+
+main();
