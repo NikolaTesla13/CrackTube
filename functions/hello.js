5fcf4eea3f799be8a580d32e97b1056ecbdcf78f
Asandei Stefan <stefan.asandei@protonmail.com>
2021-12-01 09:12:52 +0200

    serverless functions go brr

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,16 @@
 exports.handler = async function (event, context) {
   const YouTube = require("youtube-sr").default;
-  const results = await YouTube.search("fireship", { limit: 3 });
+  const results = await YouTube.search("fireship", { limit: 12 });
+  const data = [];
+  for (let i = 0; i < 12; i++) {
+    data.push({
+      title: results[i].title,
+      thumbnail: results[i].thumbnail.url,
+      id: results[i].id,
+    });
+  }
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: results }),
+    body: JSON.stringify({ data: data }),
   };
 };
