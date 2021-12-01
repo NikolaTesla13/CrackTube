4ddae28e72c6ec8df219dcc0e8e85a8da1e4e9bf
Asandei Stefan <stefan.asandei@protonmail.com>
2021-12-01 09:01:05 +0200

    hmm some serverless stuff ig

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,8 @@
 exports.handler = async function (event, context) {
+  const YouTube = require("youtube-sr").default;
+  const results = await YouTube.search("fireship", { limit: 3 });
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: results }),
   };
 };
